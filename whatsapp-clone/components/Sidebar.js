0e69364446573b0c1a1bcd40c9b58db70169e0f6
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-09-09 11:57:58 +0530

    remove unnessary css files

---
+++ whatsapp-clone/components/Sidebar.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function Sidebar() {
+  return (
+    <Container>
+      <h1>Side Bar</h1>
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
